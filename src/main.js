19c61a706045ba01da7e9b355cf8f142fa551122
Denys Skorov <den.scorov1@gmail.com>
2022-03-10 11:55:41 +0200

    fix main js

--- src/main.js
+++ src/main.js
@@ -1,32 +1,19 @@
 import Vue from 'vue'
-import {BootstrapVue, IconsPlugin} from 'bootstrap-vue'
+import App from './App.vue'
 
 import axios from 'axios'
 import VueAxios from 'vue-axios';
 
-// eslint-disable-next-line no-unused-vars
-import Mapbox from "mapbox-gl"
-// eslint-disable-next-line no-unused-vars
-import { MglMap } from "v-mapbox"
+import {BootstrapVue, IconsPlugin} from 'bootstrap-vue'
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
-import 'mapbox-gl/dist/mapbox-gl.css'
-import 'v-mapbox/dist/v-mapbox.css';
-
-import App from './App.vue'
-
-
 
 Vue.config.productionTip = false
 
 Vue.use(BootstrapVue)
-// Optionally install the BootstrapVue icon components plugin
 Vue.use(IconsPlugin)
-
 Vue.use(VueAxios, axios)
-
-
 
 new Vue({
     render: h => h(App),
