c88e09cde87a5f99d5365cbc193f650dfd229c3e
Denys Skorov <den.scorov1@gmail.com>
2022-03-11 16:14:45 +0200

    fix map click

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@
         <div class="col-4">
           <b-select :options="['blue','red']" v-model="color"/>
           <b-list-group>
-            <b-list-group-item ref="groupItem" v-for="user of correctUsers" :key="user.id"
+            <b-list-group-item ref="groupItem" v-for="user of users" :key="user.id"
                                @mouseover="userHover(user)">
               <p>{{ user.name }}</p>
               <p>{{ user.address.geo }}</p>
@@ -14,10 +14,10 @@
         </div>
         <div class="col-8">
           <MglMap ref="map" container="map-test" :zoom="1" :center="[-87.661557, 41.893748]" :accessToken="accessToken"
-                  :mapStyle="mapStyle" @load="loadMap"
+                  :mapStyle="mapStyle" @load="loadMap" @click="mapClick"
           >
-            <MglMarker v-for="user of correctUsers" :key="user.id"
-                       :coordinates="[user.address.geo.lat, user.address.geo.lng]"
+            <MglMarker v-for="user of users" :key="user.id"
+                       :coordinates="[user.address.geo.lng, user.address.geo.lat]"
                        @added="addMarker($event, user)" :ref="`marker${user.id}`">
               <template slot="marker">
                 <div>123456</div>
@@ -61,15 +61,18 @@ export default {
         .get('https://jsonplaceholder.typicode.com/users')
         .then(response => {
           this.users = response.data
+
+          // this.users[0].address.geo.lat =  49.408828
+          // this.users[0].address.geo.lng =27.0185571
         })
   },
   computed: {
-    correctUsers() {
-      return this.users.filter(user => user.address.geo.lat <= 90 &&
-          user.address.geo.lat >= -90 &&
-          user.address.geo.lng <= 90 &&
-          user.address.geo.lng >= -90)
-    }
+    // correctUsers() {
+    //   return this.users.filter(user => user.address.geo.lat <= 90 &&
+    //       user.address.geo.lat >= -90 &&
+    //       user.address.geo.lng <= 90 &&
+    //       user.address.geo.lng >= -90)
+    // }
   },
   updated() {
     this.$refs.map.map?.resize()
@@ -77,6 +80,10 @@ export default {
   methods: {
     loadMap({map}) {
       this.map = map
+    },
+    mapClick({mapboxEvent: {lngLat: {lat, lng}}}) {
+      this.users[0].address.geo.lat = lat
+      this.users[0].address.geo.lng = lng
     },
     // showInfo($event, user) {
     // },
