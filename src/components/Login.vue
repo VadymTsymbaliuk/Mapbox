607738dae1839f4e25e9a16c4cda96f8b19d7ce2
vadimcimbalyk <vadymonua@gmail.com>
2022-03-18 16:35:58 +0200

    add authorisation

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,8 +1,5 @@
 <template>
   <div class="container">
-    <b-toast id="example-toast" title="BootstrapVue" static no-auto-hide>
-      Hello, world! This is a toast message.
-    </b-toast>
     <div class="row justify-content-center">
       <div class="col-md-8">
         <div class="card">
@@ -12,7 +9,7 @@
             <form action="#" @submit.prevent="submit">
               <div class="form-group row justify-content-center">
                 <div class="col-md-6 mb-3">
-                  <label for="name" class="col-md-4 col-form-label text-md-right">Name</label>
+                  <label for="name" class="col-form-label text-md-right">Name</label>
 
                   <input
                       id="name"
@@ -26,8 +23,8 @@
                   />
                 </div>
                 <div class="col-md-8 d-flex justify-content-around ">
-                  <b-button variant="outline-success" type="submit">Login</b-button>
-                  <b-button variant="outline-primary" @click="$router.push('/registration')">Sing In</b-button>
+                  <b-button variant="outline-success" @click.stop="login">Login</b-button>
+                  <b-button variant="outline-primary" @click="$router.push('/registration')">Sign up</b-button>
                 </div>
               </div>
 
@@ -57,20 +54,33 @@ export default {
   },
 
   methods: {
-    submit() {
+    login() {
+
       db.collection('users')
           .where('name', '==', this.form.name)
           .get()
           .then(querySnapshot => {
             this.user = querySnapshot.docs.map(doc => {
               return {...doc.data(), storeId: doc.id}
-            })
-
+            })[0]
+            if (!this.user) {
+              this.makeToast(true)
+            } else {
+              localStorage.setItem('auth', JSON.stringify(this.user))
+              this.$router.push({name: 'home'})
+            }
           })
-      if (!this.user) {
-        this.$bvToast.show('example-toast')
-      }
-    }
+
+    },
+    // eslint-disable-next-line no-unused-vars
+    makeToast(append = false) {
+      // console.log(this.$bvToast)
+      this.$bvToast.toast(`This is toast number`, {
+        title: 'BootstrapVue Toast',
+        // autoHideDelay: 5000,
+        // appendToast: append
+      })
+    },
   }
 }
 </script>
